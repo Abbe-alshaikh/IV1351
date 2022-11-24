7f1e87348a99deac1ab77fe6e58c48ff5403931b
Abbe-alshaikh <55236999+Abbe-alshaikh@users.noreply.github.com>
2022-11-24 22:30:29 +0100

    a few test insertions

---
+++ insertion.sql
@@ -0,0 +1,4 @@
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Alex Peterson', 
+9504076655, 27, 'Uppsala', 12345, 'Sveavägen 44', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Alex.Peterson@google.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('082114477','07427654311');
