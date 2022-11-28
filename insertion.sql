2c8a2fc3778fddb47ff2649d888d087d7aca38ee
Abbe-alshaikh <55236999+Abbe-alshaikh@users.noreply.github.com>
2022-11-28 22:11:45 +0100

    Update insertion.sql
    adding more students-instructors

--- insertion.sql
+++ insertion.sql
@@ -2,3 +2,77 @@ INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) V
 9504076655, 27, 'Uppsala', 12345, 'Sveavägen 44', 'student');
 INSERT INTO email (eamil_adress) VALUES ('Alex.Peterson@google.com');
 INSERT INTO telephone_number (number, parent_number) VALUES ('082114477','07427654311');
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Ahmed Sade',8811134532,33,'Malmö',44345,'Norsvägen 23','student');
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('David Jonsson',  8817734532, 33, 'Malmö', 44345, 'Torsvägen 13', 'student');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('David Jonsson',  8817734532, 33, 'Malmö', 44345, 'Torsvägen 13', 'student');
+INSERT INTO email (eamil_adress) VALUES ('David.Jonsson@google.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('085567377','07423934111');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Adam Daa',  9117734532, 31, 'Torpshammar', 23245, 'Solgatan 13', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Adam.Daa@google.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('085098777','07423965431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Yasin Byn',  0117734532, 31, 'Rinkeby', 25545, 'Rinkebysvängen 13', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Yasin.Byn@google.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('085329577','07420915431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Donna Roma',  0117734532, 31, 'Fagersta', 25545, 'Faggatan 10', 'instructor');
+INSERT INTO email (eamil_adress) VALUES ('Donna.Roma@google.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('085548077','07423260431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Siko Rad',  0010044532, 31, 'Stockholm', 18545, 'Ringvägen 10', 'instructor');
+INSERT INTO email (eamil_adress) VALUES ('Siko.Rad@google.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('085422077','07426655431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Daniel Mohammed',  0010044532, 31, 'Stockholm', 18545, 'Storgatan 54', 'instructor');
+INSERT INTO email (eamil_adress) VALUES ('Daniel.Mohammed@google.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('085011077','07423119431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Franco Svensson',  0010044532, 31, 'Stockholm', 18545, 'Parkvägen 5', 'instructor');
+INSERT INTO email (eamil_adress) VALUES ('Franco.Svensson@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0729533583','07432853431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Franco Svensson',  0010044532, 31, 'Stockholm', 18545, 'Parkvägen 5', 'instructor');
+INSERT INTO email (eamil_adress) VALUES ('Franco.Svensson@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0729533583','07432853431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Adam Bogba',  02144532, 31, 'Stockholm', 18545, 'Parkvägen 7', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Adam.Bogba@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0728888583','07332263431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Ronny Bogba',  02144532, 16, 'Stockholm', 18545, 'Parkvägen 7', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Ronny.Bogba@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0758888583','07432263431');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Jan berg',  02144532, 14, 'Stockholm', 18545, 'Sundgatan 7', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Jan.Berg@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0733883283','07112252831');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Tove berg',  02051532, 14, 'Stockholm', 18545, 'Badberg 7', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Jan.Berg@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0734725283','07121742831');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Maria Maria',  92051532, 14, 'Stockholm', 18545, 'Latinvägen 2', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Maria.Maria@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0767341053','07121802131');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Zeinab Saadi',  93051532, 32, 'Stockholm', 16645, 'Hanstagatan 10', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Zeinab.Saadi@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0761432053','07125317331');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Katerina Mahmoud',  84051532, 42, 'Stockholm', 18845, 'Sveavägen 11', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Katerina.Mahmoud@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('07650801053','07549007331');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Katerina Adamson',  95021532, 25, 'Stockholm', 18945, 'Sveagatan 11', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Katerina.Adamson@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('07660801053','07566007331');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Ida Frediksson',  921210132, 29, 'Stockholm', 15945, 'Strandvägen 3', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Ida.Freddie@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('07538901053','07512842031');
+
+INSERT INTO contacts_details (name, personal_number,age,city,zip,street, role) VALUES ('Alina Philipsand',  920610152, 30, 'Stockholm', 11145, 'Kungensgatan 8', 'student');
+INSERT INTO email (eamil_adress) VALUES ('Alina.Philipsand@hotmail.com');
+INSERT INTO telephone_number (number, parent_number) VALUES ('0788101053','07144203391');
