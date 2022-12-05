533c2e1486c93278f0c7d5f398ded56bf4f85d9d
Abbe-alshaikh <55236999+Abbe-alshaikh@users.noreply.github.com>
2022-12-05 22:08:49 +0100

    Update Task Queries.sql

--- Task Queries.sql
+++ Task Queries.sql
@@ -34,9 +34,9 @@ INNER JOIN
 ) grp ON s.family_id_for_discount = grp.family_id_for_discount;
 
 -----------------------
-List all instructors who has given more than a specific number of lessons during the current month.
- Sum all lessons, independent of type, and sort the result by the number of given lessons.
- This query will be used to find instructors risking to work too much, and will be executed daily.
+-- List all instructors who has given more than a specific number of lessons during the current month.
+-- Sum all lessons, independent of type, and sort the result by the number of given lessons.
+-- This query will be used to find instructors risking to work too much, and will be executed daily.
 
 SELECT * from (SELECT COUNT(*) as number_of_lessons,instructor_id as instructor from lesson
     WHERE to_char(date, 'YYYY-MM')
